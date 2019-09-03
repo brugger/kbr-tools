0f5ccdbb23d96a77766aa1a37989fb3e0e1ff474
brugger <kim.brugger@uib.no>
2019-09-03 19:56:46 +0200

    updated setup.py module to include the shared files. Still need to push the html-templates as well

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
+import os
 from setuptools import setup
+
 def readme():
     with open('README.rst') as f:
         return f.read()
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            if filename.endswith("~"):
+                continue
+            paths.append(os.path.join(path, filename))
+    print( paths )
+    return paths
+
 
 setup(name='kbr',
       version='0.0.1',
@@ -20,8 +33,12 @@ setup(name='kbr',
         'License :: MIT License',
         'Programming Language :: Python :: 3'
         ],      
-#      scripts=[],
+      scripts=['bin/angular_code_gen.py',
+               'bin/daemon_check.py',
+                ],
       # install our config files into an ehos share.
 #      data_files=[],
+      data_files=[('share/kbr-tools/', package_files('share/'))],
+
       include_package_data=True,
       zip_safe=False)
