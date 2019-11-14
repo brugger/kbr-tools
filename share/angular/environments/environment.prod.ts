07dc1ac1732d0fd597628d21d9c67ee9f382a522
brugger <kim.brugger@uib.no>
2019-11-14 10:12:24 +0100

    cosmetic changes

--- share/angular/environments/environment.prod.ts
+++ share/angular/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: true,
   api_base: `https://${project}./api`,
   url_base: `https://${project}.${domain}`,
-  login_url: `https://${project}.${domain}/api/authorize?response_type=token&client_id=abc&scope=scope_write&redirect_uri=' + encodeURIComponent('https://${project}.${domain}/#/login`)
+  login_url: `https://${project}.${domain}/api/authorize?response_type=token&client_id=abc&scope=scope_write&redirect_uri=` + encodeURIComponent(`https://${project}.${domain}/#/login`)
 
 
 };
