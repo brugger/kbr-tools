edca67fed5e1ccecfdb84245f8281a37055118d0
brugger <kim.brugger@uib.no>
2019-11-15 11:13:43 +0100

    typo fixes

--- share/angular/environments/environment.prod.ts
+++ share/angular/environments/environment.prod.ts
@@ -1,13 +1,11 @@
 
-const project:string = "CHANGE-ME";
-const domain:string = "bruggercorp.com";
 
 import { setup } from  './setup'
 
 
 export const environment = {
   production: true,
-  api_base: `https://${project}./api`,
+  api_base: `https://${setup.domain}./api`,
   url_base: `https://${setup.domain}`,
   login_url: `https://${setup.domain}/api/authorize?response_type=token&client_id=abc&scope=scope_write&redirect_uri=` + encodeURIComponent(`https://${setup.domain}/#/login`)
 
