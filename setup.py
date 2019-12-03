46c64ae5dc8ca892d8e76f486eeabad14fbfe56c
brugger <kim.brugger@uib.no>
2019-12-03 11:27:46 +0100

    dynamic versions and added a script

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 from setuptools import setup
+import json
 
 def readme():
     with open('README.rst') as f:
@@ -15,9 +16,15 @@ def package_files(directory):
     print( paths )
     return paths
 
+def get_version():
+    with open('version.json') as json_file:
+        data = json.load(json_file)
+
+    return "{}.{}.{}".format( data['major'], data['minor'], data['patch'])
+
 
 setup(name='kbr',
-      version='0.0.1',
+      version= get_version(),
       description='python utils and tools collection',
       url='https://github.com/brugger/kbr-tools/',
       author='Kim Brugger',
@@ -32,16 +39,14 @@ setup(name='kbr',
           'psycopg2-binary',
       ],
       classifiers=[
-        'Development Status :: 0.0.1',
+        "Development Status :: 0.0.1".format( get_version()),
         'License :: MIT License',
         'Programming Language :: Python :: 3'
         ],      
       scripts=['bin/angular_code_gen.py',
                'bin/daemon_check.py',
+               'bin/kbr-dev-utils.py'
                 ],
-      # install our config files into an ehos share.
-#      data_files=[],
       data_files=[('share/kbr-tools/', package_files('share/'))],
-
       include_package_data=True,
       zip_safe=False)
