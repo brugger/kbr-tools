7d1edfe5e7832533fd30276cdbe2854949793506
brugger <kim.brugger@uib.no>
2020-06-30 11:36:45 +0200

    added an email regex matcher line

--- kbr/string_utils.py
+++ kbr/string_utils.py
@@ -1,4 +1,7 @@
 
+email_match = '([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})'
+email_regex = r'^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$'
+
 
 def comma_sep(elements:[]) -> str:
     return ", ".join( map(str, elements))
