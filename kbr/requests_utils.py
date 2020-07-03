6f22272b576e08d973ef119885513cdb2dc060f5
brugger <kim.brugger@uib.no>
2020-07-03 12:00:54 +0200

    loads of fixes

--- kbr/requests_utils.py
+++ kbr/requests_utils.py
@@ -8,10 +8,27 @@ def set_token(new_token:str):
     global token
     token = new_token
 
-def request_call(url:str, as_json:bool=True):
+def get(url:str, as_json:bool=True):
+    return generic_request(url, as_json, call='GET')
+
+def post(url:str, data:{}):
+    return generic_request(url, call='POST', data=data)
+
+def patch(url:str, data:{}):
+    return generic_request(url, call='PATCH', data=data)
+
+def delete(url:str, data:{}):
+    return generic_request(url, call='DELETE', data=data)
+
+
+
+def generic_request(url:str, as_json:bool=True, call='GET', data:{}=None):
 
     s = Session()
-    req = Request('GET',  url, )
+    if as_json and data is not None:
+        req = Request(call,  url, json=data)
+    else:
+        req = Request(call,  url, data=data)
 
     prepped = s.prepare_request(req)
 
@@ -24,15 +41,10 @@ def request_call(url:str, as_json:bool=True):
     r.raise_for_status()
 
     token = None
+    if 'Authorization' in r.headers:
+        token = r.headers['Authorization'][7:]
 
-    if as_json:
-        if 'Authorization' in r.headers:
-            token = r.headers['Authorization'][7:]
-
+    if as_json and r.text:
         return json.loads( r.text ), token
     else:
         return r.text, token
-
-
-def get_info(instance:str) -> {}:
-    return request_call(f"{instance}/info")
