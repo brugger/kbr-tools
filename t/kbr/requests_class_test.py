e0b0885f991cd32b91eea3cf6786a10bcbc29317
brugger <kim.brugger@uib.no>
2020-08-27 08:36:53 +0200

    test for the new requests class

--- t/kbr/requests_class_test.py
+++ t/kbr/requests_class_test.py
@@ -1,39 +1,44 @@
 import unittest
 
 
-import kbr.requests_utils as requests
+import kbr.requests_class as requests_class
 
 instance = 'http://localhost:8088/'
 token    = 'my_secret'
 
 def test_set_token():
-    requests.set_token( token )
+    r = requests_class.KBRRequests(base_url=instance)
+    r.set_token( token )
 
 
 def test_request_get():
 
-    requests.set_token( token )
-    response, return_token = requests.get(instance)
+    r = requests_class.KBRRequests(base_url=instance)
+    r.set_token( token )
+    response, return_token = r._get()
 
     assert return_token == token
 
 def test_request_post():
 
-    requests.set_token( token )
-    response, return_token = requests.post(instance, {'user': 'me'})
+    r = requests_class.KBRRequests(base_url=instance)
+    r.set_token( token )
+    response, return_token = r._post()
 
     assert return_token == token
 
 def test_request_patch():
 
-    requests.set_token( token )
-    response, return_token = requests.patch(instance, {'user': 'me'})
+    r = requests_class.KBRRequests(base_url=instance)
+    r.set_token( token )
+    response, return_token = r._patch()
 
     assert return_token == token
 
 def test_request_delete():
 
-    requests.set_token( token )
-    response, return_token = requests.delete(instance, {'user': 'me'})
+    r = requests_class.KBRRequests(base_url=instance)
+    r.set_token( token )
+    response, return_token = r._delete()
 
     assert return_token == token
