5795768737140b4a92b8e2e7203eeb8541dd1251
brugger <kim.brugger@uib.no>
2020-08-31 09:37:55 +0200

    new to_str function, needs some more work

--- kbr/string_utils.py
+++ kbr/string_utils.py
@@ -8,3 +8,11 @@ domain_match = '[a-zA-Z0-9_\-\.]+\.[a-zA-Z]{2,5}'
 
 def comma_sep(elements:[]) -> str:
     return ", ".join( map(str, elements))
+
+
+def to_str(value:any) -> str:
+
+    if instance(value, bytearray):
+        return value.decode("utf-8")
+
+    return str( value )
