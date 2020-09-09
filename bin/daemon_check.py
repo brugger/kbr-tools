8991283d27e22cf6eb54fa9c209178509953699d
brugger <kim.brugger@uib.no>
2020-09-09 13:44:37 +0200

    more advanced version, can run by itself instead of through cron

--- bin/daemon_check.py
+++ bin/daemon_check.py
@@ -13,12 +13,20 @@ pp = pprint.PrettyPrinter(indent=4)
 import argparse
 import subprocess
 import shlex
+import time
+
+import psutil
+
+sys.path.append(".")
+
+import kbr.config_utils as config_utils
+import kbr.json_utils as json_utils
 
 # python3+ is broken on centos 7, so add the /usr/local/paths by hand
 sys.path.append("/usr/local/lib/python{}.{}/site-packages/".format( sys.version_info.major, sys.version_info.minor))
 sys.path.append("/usr/local/lib64/python{}.{}/site-packages/".format( sys.version_info.major, sys.version_info.minor))
 
-import psutil
+
 
 def find_procs_by_name(name):
     "Return a list of processes matching 'name'."
@@ -27,6 +35,18 @@ def find_procs_by_name(name):
         if p.info['name'] == name:
             ls.append(p)
     return ls
+
+
+def write_example_config():
+    data = [['command-name', 'command', 'count'], ['command-name-2', 'command-2', 'count-2']]
+    filename = "daemon_check.json"
+    json_utils.write( filename, data )
+    print( f"Example config file written to {filename}")
+
+
+
+def readin_config(config):
+    return json_utils.read(config)
 
 def main():
     """ main loop
@@ -41,22 +61,51 @@ def main():
       None
     """
 
+    checks = []
+
     parser = argparse.ArgumentParser(description='Daemon checker, if not running re-start it ')
 
-    parser.add_argument('-n', '--name',     required=True, help="Name to check for")
-    parser.add_argument('-c', '--command',  required=True,help="command to run if name is not found")
-    parser.add_argument('-N', '--number',  required=False, default=1,help="number of processes to be running")
+    parser.add_argument('-n', '--name',     help="Name to check for")
+    parser.add_argument('-c', '--command',  help="command to run if name is not found")
+    parser.add_argument('-N', '--number', type=int, default=1, help="number of processes to be running")
+    parser.add_argument('-C', '--config',  help="json file for multiple processes")
+    parser.add_argument('-X', '--example-config', action="store_true", default=False,   help="creates an example config file")
+    parser.add_argument('-s', '--sleep', type=int, default=0, help="to have it run continually set sleep")
 
 
     args = parser.parse_args()
-    args.number = int( args.number )
+    sleep = args.sleep
 
-    ls = find_procs_by_name( args.name )
-    running = len( ls )
-    if ( running < args.number ):
-        for _ in range(0, args.number - running ):
-            subprocess.Popen(shlex.split( args.command ), shell=True)
-    
+    if args.example_config:
+        write_example_config()
+        sys.exit()
+    elif args.config is not None:
+        checks = readin_config( args.config )
+    elif args.command is not None or args.name is not None:
+        if args.command is None or args.name is None:
+            print("Error: argument missing -c<ommand> -n<ame> -N[number of proceses]")
+            sys.exit( -1 )
+
+        checks.append( [args.name, args.command, args.number])
+    else:
+        parser.print_usage()
+        sys.exit( 1 )
+
+
+    while True:
+        for check in checks:
+            name, command, number = check
+            number = int( number )
+            ls = find_procs_by_name( name )
+            running = len( ls )
+            if ( running < number ):
+                for _ in range(0, number - running ):
+                    subprocess.Popen(shlex.split( command ), shell=True)
+        if sleep == 0:
+            break
+
+        time.sleep( sleep )
+
 
 
 if __name__ == '__main__':
