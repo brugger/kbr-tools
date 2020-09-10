6e3b9d591db270fbebc00e97a65c795165638fb8
brugger <kim.brugger@uib.no>
2020-09-10 11:30:25 +0200

    renamed and added some new functions

--- kbr/timedate_utils.py
+++ kbr/timedate_utils.py
@@ -15,12 +15,10 @@ import datetime
 
 
 
-def now():
+def now() -> float:
     """ retuns current time as seconds inc microseconds """
 
     return time.time()
-    
-    return  datetime.datetime.now()
 
 def to_string(ts):
     """ Convert a time to string, if an int will transform it to a timestamp before returning the string"""
@@ -36,17 +34,68 @@ def to_string(ts):
 
 
 
-def to_sec( ts ):
+def to_sec_since_epoch( ts ):
 
-        
     if isinstance(ts, int) or isinstance(ts, float):
         return int(ts)
     elif isinstance( ts, datetime.datetime ):
         return  int( ts.timestamp() )
     elif isinstance( ts, str):
         return datetime.time.fromisoformat( ts )
-#        return str(datetime.datetime.fromisoformat( ts ))
-#        return int( ts )
     else:
         raise ValueError("Cannot convert {} to an int".format( ts))
 
+def to_days_since_epoch(ts:int) -> int:
+    timestamp = datetime.datetime.fromtimestamp(ts)
+    stime = "{}-{}-{} 00:00:00".format( timestamp.year, timestamp.month, timestamp.day )
+
+    return datetime.datetime.strptime(stime, "%Y-%m-%d %H:%M:%S").timestamp()
+
+def to_months_since_epoch(ts:int) -> int:
+    timestamp = datetime.datetime.fromtimestamp(ts)
+    stime = "{}-{}-{} 00:00:00".format( timestamp.year, timestamp.month, timestamp.day )
+
+    return datetime.datetime.strptime(stime, "%Y-%m-%d %H:%M:%S").timestamp()
+
+
+def datestr_to_ts(datetime_str:str) -> any:
+
+    for time_string in ["%Y-%m-%dT%H:%M:%S.%f%z",
+                        "%Y-%m-%dT%H:%M:%S.%f",
+                        "%Y-%m-%dT%H:%M:%S%z",
+                        "%Y-%m-%dT%H:%M:%S",
+                        "%Y-%m-%d %H:%M:%S.%f%z",
+                        "%Y-%m-%d %H:%M:%S.%f",
+                        "%Y-%m-%d %H:%M:%S%z",
+                        "%Y-%m-%d %H:%M:%S"]:
+        try:
+            ts = datetime.datetime.strptime(datetime_str, time_string)
+            return ts
+        except:
+            pass
+
+
+    raise RuntimeError(f"cannot convert datetime string '{datetime}' to timestamp ")
+
+
+
+def epoch_to_timestr(time:int):
+    ts = datetime.datetime.fromtimestamp(time)
+    return ts
+
+
+def weekday(timestamp:str) -> []:
+
+    try:
+        ts = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f%z")
+    except:
+        try:
+            ts = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S%z")
+        except:
+            print(f"Cannot convert timestamp {timestamp} to weekday")
+
+    return ts.weekday(), ts.hour, ts.minute
+
+
+#def time_delta(ts:int)
+#    tdelta = datetime.strptime(s2, FMT) - datetime.strptime(s1, FMT)
