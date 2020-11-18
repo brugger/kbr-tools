5e7be60c0aa9c2d053e1116b1a9e49a3e62a98f7
brugger <kim.brugger@uib.no>
2020-11-18 10:30:37 +0100

    prog to check http endpoints

--- bin/http_status.py
+++ bin/http_status.py
@@ -5,21 +5,16 @@
 
  Kim Brugger (25 Sep 2020), contact: kim@brugger.dk
 """
-import socket
 import argparse
 import sys
 import re
-import tabulate
+import json
 import requests
 
-def get_host_name() -> str:
-    return socket.getfqdn()
 
 def get_state(service) -> int:
 
     r = requests.get(service)
-
-
     return {'code':r.status_code, 'response_time':r.elapsed.total_seconds()}
 
 
@@ -29,10 +24,9 @@ def main():
     parser.add_argument('-t', '--telegraf', default=False, action="store_true",    help="telegraf compatible format")
     parser.add_argument('-j', '--json', default=False, action="store_true",    help="telegraf compatible format")
     parser.add_argument('-n', '--name',  help="service name (otherwise hostname)")
-    parser.add_argument('services', nargs='*', help="service(s) to check")
+    parser.add_argument('services', nargs='*', help="service(s) to check (use pattern name=http:// to substitute hostname)")
 
     args = parser.parse_args()
-
 
     if len(args.services) == 0:
         print("No service(s) provided to check!")
@@ -46,20 +40,34 @@ def main():
     statuses = []
 
     for service in args.services:
+        #set the service name
+        m = re.match(r'(.*?)=(.*?//.*)', service)
+        if args.name is not None:
+            name = args.name
+        elif m:
+            name, service = m.groups()
+        else:
+            name =re.sub(r'^.*?//', '', service)
+            name =re.sub(r'(.*?)/.*$', r'\1', name)
+
         status = get_state(service)
-        service =re.sub(r'^.*?//', '', service)
-        service =re.sub(r'(.*?)/.*$', r'\1', service)
+
         if args.telegraf:
-            line = f"service,service={service} status_code={status['code']},response_time={status['response_time']}"
+            line = f"service,service={name} status_code={status['code']},response_time={status['response_time']}"
             print( line )
         else:
+            status['name'] = name
             statuses.append( status )
 
 
     if args.json:
         print(json.dumps( statuses))
     elif not args.telegraf:
-        print(tabulate.tabulate( statuses, headers={'status_code': 'code'}, tablefmt='psql'))
+
+        print(f"{'name':20}\tcode\tresponse_time")
+        print(f"--------------------------------------")
+        for status in statuses:
+            print(f"{status['name']:20}\t{status['code']}\t{status['response_time']}")
 
 
 
