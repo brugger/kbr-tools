d496ad0954169a4f1f02d5818ef7ab9fbf228342
brugger <kim.brugger@uib.no>
2021-02-24 09:34:43 +0100

    missed bugs

--- kbr/type_utils.py
+++ kbr/type_utils.py
@@ -3,11 +3,11 @@ def is_string( value ) -> bool:
 
 
 def is_int( value ) -> bool:
-    return isinstance(s, int)
+    return isinstance(value, int)
 
 
 def is_float( value ) -> bool:
-    return isinstance(s, float)
+    return isinstance(value, float)
 
 def is_number( value ) -> bool:
     return is_int( value ) or is_float( value )
