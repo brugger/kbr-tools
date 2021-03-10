b6e7eba945b83021e6f277e03e1b131ae823b15d
brugger <kim.brugger@uib.no>
2021-03-10 19:20:45 +0100

    filepath/dir

--- kbr/os_utils.py
+++ kbr/os_utils.py
@@ -4,3 +4,15 @@ import os
 def mkdirs(dirs:[]) -> None:
     for d in dirs:
         os.mkdir( d )
+
+
+
+def file_path(filename:str=None) -> str:
+    if filename is None:
+        filename = __file__
+        
+    return os.path.realpath(__file__) 
+
+
+def file_dir(filename:str=None) -> str:
+    return os.path.dirname(file_path(filename))
