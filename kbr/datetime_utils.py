95c8f8274e28b986e7fd91c8404026433488c940
Kim Brugger <kbr@brugger.dk>
2021-07-29 21:43:08 +0200

    typo

--- kbr/datetime_utils.py
+++ kbr/datetime_utils.py
@@ -17,7 +17,7 @@ pp = pprint.PrettyPrinter(indent=4)
 
 time_strings = ["%Y-%m-%dT%H:%M:%S.%f%z",
                 "%Y-%m-%dT%H:%M:%S.%f",
-                "%Y-%m-%dT%H:%M:%S.%fZ" # cromwell
+                "%Y-%m-%dT%H:%M:%S.%fZ", # cromwell
                 "%Y-%m-%dT%H:%M:%S%z",
                 "%Y-%m-%dT%H:%M:%S",
                 "%Y-%m-%d %H:%M:%S.%f%z",
