3d20715cc974adc934b6305d105e793c6d228768
brugger <kim.brugger@uib.no>
2022-07-04 09:30:19 +0200

    function to pop a value from either a list or a dict

--- kbr/misc_utils.py
+++ kbr/misc_utils.py
@@ -1,4 +1,6 @@
+import os
 import kbr.file_utils as file_utils
+import typing
 
 def to_list(value) -> list:
     if isinstance(value, list):
@@ -9,6 +11,30 @@ def to_list(value) -> list:
 def readin_if_file(name:str) -> str:
 
     if os.path.isfile( name):
-        name = file_utils.read(name)
-    return name
+        return file_utils.read(name)
+
+    return None
+
+def pop_value(key:str, container:typing.Union[dict,list]) -> typing.Tuple[any, typing.Union[dict, list]]:
+
+    value = None
+    if isinstance(container, dict):
+        if key in container:
+            value = container[key]
+            del container[key]
+
+        return value, container
+
+
+    if isinstance(container, list):
+        if key in container:
+            value = key
+            container.remove(key)
+
+        return value, container
+
+
+    raise TypeError('function accepts either a list or a dictionary as the container')        
+
+
 
