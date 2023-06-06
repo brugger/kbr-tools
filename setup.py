065efdf2e64ef8d1aaf40b610da1b54ac65f4cfd
Kim Brugger <kbr@brugger.dk>
2023-06-06 12:15:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -53,6 +53,4 @@ setup(name='kbr',
         'Programming Language :: Python :: 3'
         ],      
       scripts=scripts(),
-#      data_files=[('share/kbr-tools/', package_files('share/'))],
-#      include_package_data=True,
       zip_safe=False)
