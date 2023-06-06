e5c750643613d3b993beefabc766e1e8fbdca84b
Kim Brugger <kbr@brugger.dk>
2023-06-06 08:57:10 +0200

    Update setup.py
    no shared files, so removed

--- setup.py
+++ setup.py
@@ -61,6 +61,6 @@ setup(name='kbr',
         'Programming Language :: Python :: 3'
         ],      
       scripts=scripts(),
-      data_files=[('share/kbr-tools/', package_files('share/'))],
-      include_package_data=True,
+#      data_files=[('share/kbr-tools/', package_files('share/'))],
+#      include_package_data=True,
       zip_safe=False)
